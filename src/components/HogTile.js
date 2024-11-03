f225fbd723b061bf973aa1aa468bb6eb92e64435
abdiqafar <iabdiqafar24@gmail.com>
2024-11-03 12:57:19 +0300

    commit

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -2,22 +2,28 @@ import React from "react";
 
 const HogTile = ({ hog, onSelect, onHide }) => {
   return (
-    <div className="pigTile" onClick={() => onSelect(hog)}>
-      <img src={hog.image} alt={hog.name} />
-      <h3>{hog.name}</h3>
-      <p>{hog.specialty}</p>
-      <p>Weight: {hog.weight} kg</p>
-      <p>Greased: {hog.greased ? "Yes" : "No"}</p>
-      <p>Highest Medal: {hog["highest medal achieved"]}</p>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          onHide(hog);
-        }}
-      >
-        Hide
-      </button>
-    </div>
+    <article className="hog-tile" onClick={() => onSelect(hog)}>
+      <header>
+        <img src={hog.image} alt={hog.name} />
+        <h3>{hog.name}</h3>
+        <p>{hog.specialty}</p>
+      </header>
+      <section>
+        <p>Weight: {hog.weight} kg</p>
+        <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+        <p>Highest Medal: {hog["highest medal achieved"]}</p>
+      </section>
+      <footer>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onHide(hog);
+          }}
+        >
+          Hide
+        </button>
+      </footer>
+    </article>
   );
 };
 
